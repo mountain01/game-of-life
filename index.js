e179cc1b25028a908d814f61afe4dfc65520c7a5
Matt Edwards <edwardsjm01@gmail.com>
2018-10-17 20:31:04 +0000

    add basic grid

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const app = angular.module('Life', []);
 
+app.controller('LifeCtrl', ['$scope', function($scope) {
+  $scope.test = 'Hello there';
+  const size = 50;
+  $scope.board = new Array(size);
+  for (let i = 0; i < size; i++) {
+    $scope.board[i] = new Array(size).fill(false);
+  }
+  $scope.board[30][20] = true;
+}]);
